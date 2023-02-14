724f0991474f788885f04fdea11a859397072b0f
thagiuda <takahiro.hagiuda@gmail.com>
2023-02-15 05:30:13 +0900

    add card class

---
+++ src/card.js
@@ -0,0 +1,30 @@
+class Card {
+  // const suit = ['H', 'D', 'C', 'S'];
+  // const rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9, 10', 'J', 'Q', 'K'];
+  constructor(suit, rank) {
+    this.suit = suit;
+    this.rank = rank;
+  }
+
+  getRankNumber() {
+    if (this.rank == 'A') {
+      return 11; // 後で残りの手札を見て1か11にできるよう書き直す
+    } else if (2 <= this.rank && this.rank <= 10) {
+      return parseInt(this.rank);
+    } else {
+      return 10;
+    }
+  }
+}
+
+const card1 = new Card('H', 'A');
+console.log(card1.getRankNumber());
+
+const card2 = new Card('D', '2');
+console.log(card2.getRankNumber());
+
+const card3 = new Card('C', '10');
+console.log(card3.getRankNumber());
+
+const card4 = new Card('S', 'K');
+console.log(card4.getRankNumber());
