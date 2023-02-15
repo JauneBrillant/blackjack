2c3f19015f694f943cf6134a99bd80536051290e
thagiuda <takahiro.hagiuda@gmail.com>
2023-02-15 18:07:52 +0900

    fix bug refactoring

--- src/deck.js
+++ src/deck.js
@@ -1,23 +1,25 @@
-class Deck {
+import { Card } from "./card.js";
+
+export class Deck {
   constructor() {
-    this.deck = Deck.generateDeck();
+    this.deck = [];
+    this.generateNewDeck();
   }
 
-  static generateDeck() {
-    let newDeck = [];
-    const suit = ['H', 'D', 'C', 'S'];
-    const rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  generateNewDeck() {
+    this.deck = [];
+    const suit = ["H", "D", "C", "S"];
+    const rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 13; j++) {
-        newDeck.push(new Card(suit[i], rank[j]));
+        this.deck.push(new Card(suit[i], rank[j]));
       }
     }
-    return newDeck;
   }
 
   shuffleDeck() {
     for (let i = 0; i < 52; i++) {
-      let random = Math.floor(Math.random() * 53);
+      let random = Math.floor(Math.random() * 52);
       let temp = this.deck[i];
       this.deck[i] = this.deck[random];
       this.deck[random] = temp;
@@ -28,12 +30,6 @@ class Deck {
     return this.deck.pop();
   }
 
-  resetDeck() {
-    const deck = new Deck();
-    deck.shuffleDeck();
-    return deck;
-  }
-
   printDeck() {
     for (let i = 0; i < Object.keys(this.deck).length; i++) {
       console.log(this.deck[i]);
@@ -42,11 +38,16 @@ class Deck {
 }
 
 // --------------------- TEST -------------------------
+// console.log(typeof module);
+
 // const deck1 = new Deck();
 // deck1.printDeck();
 // deck1.shuffleDeck();
 // deck1.printDeck();
 
+// console.log(deck1);
+// console.log(Object.keys(deck1.deck).length);
 // console.log(deck1.drawOne());
-
-// deck1.resetDeck();
+// console.log(deck1.drawOne());
+// console.log(deck1.drawOne());
+// console.log(Object.keys(deck1.deck).length);
