4bc9bdc3a59bfdd959c3ae6d4e2cd122418b8839
thagiuda <takahiro.hagiuda@gmail.com>
2023-02-15 14:40:24 +0900

    add player class base

---
+++ src/player.js
@@ -0,0 +1,22 @@
+class player {
+  constructor() {}
+}
+
+// データ
+// プレイヤーのハンド
+// プレイヤーのchip
+// プレイヤーがbetした金額
+// プレイヤーの状態(bet, surrender, stand, hit, double, bust)
+
+// 挙動
+// makebet(playerのchip){
+//     return bet額;
+// };
+
+// takeaction(プレイヤの状態){
+//     betならbetする
+//     surrenderならbetした金額を失って新しいゲームに
+//     hitならもう一枚引く
+//     bustならbetした金額を失って新しいゲームに
+//     など
+// }
